47fd175da9e917eec0e3e6913ac62b24dc1c6529
Berke Baç <berkebac@gmail.com>
2022-04-21 18:26:53 +0300

    live

--- MachineLearningImageRec/ViewController.swift
+++ MachineLearningImageRec/ViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var chosenImage = CIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func changeImageClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+        
+        if let ciImage = CIImage(image: imageView.image!) {
+            chosenImage = ciImage
+            
+        }
+        recognizeImage(image : chosenImage)
+    }
+    
+    func recognizeImage(image: CIImage) {
+        
+        // 1-Request
+        do {
+            if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+                let request = VNCoreMLRequest(model: model) { vnrequest, error in
+                    if error != nil {
+                        if let results = vnrequest.results as? [VNClassificationObservation] {
+                            let topresult = results.first
+                            
+                            DispatchQueue.main.sync {
+                                
+                                let confidenceLevel = (topresult?.confidence ?? 0) * 100
+                                let rounded = Int (confidenceLevel * 100) / 100
+                                self.textLabel.text = "\(rounded)% it's \(topresult?.identifier)"
+                            }
+                        }
+                    }
+                }
+                
+                // 2-Handler
+                let handler = VNImageRequestHandler(ciImage: image)
+                DispatchQueue.global(qos: .userInteractive).async {
+                    do{
+                        try handler.perform([request])
+                    } catch {
+                        print("error")
+                    }
+                    
+                }
+                
+                
+            }
+        }
+        
+        
+        
+    }
+    
 }
 
